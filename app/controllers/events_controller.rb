7fc64e3897d3476efe19e737fe743303d931f2c4
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-09-28 16:40:04 -0700

    Add #show, #new, #create to EventsController and uses association helper #build method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,30 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @event = @user.events.build
+  end
+
+  def create
+    @user = current_user
+    @event = @user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :start_date, :location)
+  end
 end
