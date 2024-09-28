c02c6fc2edf915572aea96cad80ff422b5a5ed41
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-09-27 21:40:30 -0700

    Add association between the event creator (a User) and the event

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_28_013948) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_28_041305) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.date "start_date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_28_013948) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
