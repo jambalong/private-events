d0f0fb1f8508ae118ab42ab465de17770032c6d7
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-09-29 00:01:41 -0700

    Add two class methods on Event model #past and #upcoming that returns past or upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,14 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: "attended_event_id"
   has_many :attendees, through: :attendances
+
+  def self.past
+    today = Date.today
+    self.all.where("start_date < ?", today)
+  end
+
+  def self.upcoming
+    today = Date.today
+    self.all.where("start_date >= ?", today)
+  end
 end
